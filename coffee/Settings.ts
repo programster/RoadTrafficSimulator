d2913fef0cc7f905e520b51c58641df911e46222
Programster <sdpagent@gmail.com>
2017-03-06 00:15:35 +0000

    adding the settings file which should have been added ages ago

---
+++ coffee/Settings.ts
@@ -0,0 +1,23 @@
+
+class Settings
+{
+    public static colors = {
+        background: '#97a1a1',
+        redLight: 'hsl(0, 100%, 50%)',
+        greenLight: '#85ee00',
+        intersection: '#586970',
+        road: '#586970',
+        roadMarking: '#bbb',
+        hoveredIntersection: '#3d4c53',
+        tempRoad: '#aaa',
+        gridPoint: '#586970',
+        grid1: 'rgba(255, 255, 255, 0.5)',
+        grid2: 'rgba(220, 220, 220, 0.5)',
+        hoveredGrid: '#f4e8e1'
+    };
+        
+    public static fps = 30;
+    public static lightsFlipInterval = 160;
+    public static gridSize = 14;
+    public static defaultTimeFactor = 5;
+}
